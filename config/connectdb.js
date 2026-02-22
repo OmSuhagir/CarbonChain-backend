d175543c889965e41a9f1af1f5f94d301ae34af4
OmSuhagir <omsuhagir123@gmail.com>
2026-02-22 06:09:32 +0530

    mongo

--- config/connectdb.js
+++ config/connectdb.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
  */
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/carbonchain_pro';
+    const mongoURI = process.env.MONGODB_URI;
 
     await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
